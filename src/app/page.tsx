e4949782b189ecc4066c806d41da618d4e822035
Richard Senger <richard.senger27@icloud.com>
2025-07-08 01:53:51 +0200

    Techstack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/home/HeroSection";
 import QuoteSection from "@/components/home/QuoteSection";
+import TechStack from "@/components/home/TechStack";
 
 export default function Home() {
   return (
     <main className="h-screen overflow-scroll bg-black snap-y snap-mandatory">
       <HeroSection />
       <QuoteSection />
+      <TechStack />
     </main>
   );
 }
