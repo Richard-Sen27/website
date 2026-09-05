97a62502c5626b6435c89c464912c1ef3e584895
Richard Senger <richard.senger27@icloud.com>
2026-09-05 21:37:24 +0100

    Revert "fix(scroll): stop route changes landing part way down the page"
    This reverts commit be17fbcd2800a788be4d7ba6ae29c9246662a5a7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import "./globals.css";
 
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import ScrollManager from "@/components/ScrollManager";
 import { geistMono, geistSans } from "@/lib/fonts";
 import { getGlobal, objectList, type NavLink } from "@/lib/content";
 import { site } from "@/lib/site";
@@ -63,8 +62,6 @@ export default async function RootLayout({
         >
           Skip to content
         </a>
-
-        <ScrollManager />
 
         <Header links={navLinks} siteName={global.get("siteName")} />
 
